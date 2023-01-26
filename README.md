dbfdaea7601a3196770f273146a1c62b38d8bfd0
Princerwtt <109853314+Princerwtt@users.noreply.github.com>
2023-01-26 21:58:46 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# piya-calC
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-j7xfuf)
---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('Hello!');
+let button= document.getElementById("d")
+button.onclick=()=>{
+  let a=document.getElementById("a").value;
+  let b=document.getElementById("b").value;
+let operation=document.getElementById('dropdown').value;
+let output=null;
+
+}
