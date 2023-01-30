aa69d8c98eac4051ce834b10ead8fa3628cfccce
Princerwtt <109853314+Princerwtt@users.noreply.github.com>
2023-01-30 14:10:40 +0530

    2 files modified

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 console.log('Hello!');
-let button= document.getElementById("d")
-button.onclick=()=>{
-  let a=document.getElementById("a").value;
-  let b=document.getElementById("b").value;
-let operation=document.getElementById('dropdown').value;
-let output=null;
-
-}
+let button = document.getElementById('d');
+button.onclick = () => {
+  let a = document.getElementById('a').value;
+  let b = document.getElementById('b').value;
+  let operation = document.getElementById('dropdown').value;
+  let output = null;
+  switch (operation) {
+    case '+':
+      output = parseInt(a) + parseInt(b);
+      break;
+    case '-':
+      output = a - b;
+      break;
+    case '*':
+      output = a * b;
+      break;
+    case '/':
+      output = a / b;
+      break;
+  }
+  document.getElementById('output').innerText = output;
+};
